40e566ba2e348b061a3d17be7808f0df9f847053
Matt Lindsay <Matt.Lindsay@wokingham.gov.uk>
2020-05-07 14:19:02 +0100

    Delete action.

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            context = _context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Item> items = _context.Items.ToList();
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            _context.Items.Remove();
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
